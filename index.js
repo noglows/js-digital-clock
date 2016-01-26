d90dd160e4d4c5854644ecc2a54bbc80e2959aaf
noglows <jessica.noglows@outlook.com>
2016-01-25 16:52:19 -0800

    International clocks sort of working

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ var currentTime = function() {
   dayWeek = dayNames[dayWeek]
 
   var hour = now.getHours();
+
+  // var bostonHour = hour + 3;
+  // var parisHour = hour + 9;
+  // var kyotoHour = hour + 17;
+  // var melbourneHour = hour + 19;
+
   if (hour > 12) {
     hour = hour - 12;
   }
@@ -29,11 +35,47 @@ var currentTime = function() {
     seconds = "0" + seconds;
   }
 
+  var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+
+  boston_hour = now_utc.getHours() - 5;
+  if (boston_hour < 0) {
+    boston_hour = 12 + boston_hour;
+  } else if (boston_hour > 12) {
+    boston_hour = boston_hour - 12;
+  }
+
+  paris_hour = now_utc.getHours() + 1;
+  if (paris_hour < 0) {
+    paris_hour = 12 + paris_hour;
+  } else if (paris_hour > 12) {
+    paris_hour = paris_hour - 12;
+  }
+
+  kyoto_hour = now_utc.getHours() + 9;
+  if (kyoto_hour < 0) {
+    kyoto_hour = 12 + kyoto_hour;
+  } else if (kyoto_hour > 12) {
+    kyoto_hour = kyoto_hour - 12;
+  }
+
+  melbourne_hour = now_utc.getHours() + 11;
+  if (melbourne_hour < 0) {
+    melbourne_hour = 12 + melbourne_hour;
+  } else if (melbourne_hour > 12) {
+    melbourne_hour = melbourne_hour - 12;
+  }
+
+
   document.getElementById('second').innerHTML = seconds;
   document.getElementById('minute').innerHTML = mins + ":";
   document.getElementById('hour').innerHTML = hour + ":";
-  document.getElementById('date').innerHTML = month + " " + day + ", " + year
-  document.getElementById('day').innerHTML = dayWeek
+  document.getElementById('date').innerHTML = month + " " + day + ", " + year;
+  document.getElementById('day').innerHTML = dayWeek;
+
+  document.getElementById('boston-time').innerHTML = boston_hour + ":" + mins + ":" + seconds;
+  document.getElementById('paris-time').innerHTML = paris_hour + ":" + mins + ":" + seconds;
+  document.getElementById('kyoto-time').innerHTML = kyoto_hour + ":" + mins + ":" + seconds;
+  document.getElementById('melbourne-time').innerHTML = melbourne_hour + ":" + mins + ":" + seconds;
 };
 
 setInterval(function() { currentTime(); }, 1000);
